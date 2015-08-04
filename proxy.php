ed459b33b1b8390a96db3b3652a9ad9a49f2ca79
Carsten Thue-Bludworth <cmt227@cornell.edu>
2015-08-04 12:24:54 -0400

    removed log file

--- proxy.php
+++ proxy.php
@@ -3,8 +3,6 @@
 $host = 'https://script.google.com/macros/s/AKfycbyD-AO9qTpvDhLdadZl_YU9Xec0n3YkOZwteAE3qWMNLIDcV6SU/exec'
         . '?' . http_build_query($_GET);
 
-file_put_contents('proxy.log', $host, FILE_APPEND);
-
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_URL => $host,
