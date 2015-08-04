c6d6847bd2910094d9b2b2b9ca835c4d8d43835d
Carsten Thue-Bludworth <cmt227@cornell.edu>
2015-08-04 15:50:04 -0400

    fixed some browser compatability and extended for multiple users]

--- js/calendar.js
+++ js/calendar.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
 
+    // setup a timeout to handle errors on 
+    $.ajaxSetup({
+        timeout: 1000
+    });
+
     // Important DOM elements
     var cal = $('#calendar');
     var mod = $('#inputModal');
     var text = $('#booking-text');
     var form = $('#booking-form');
 
-    // The start and end times of the appointment being scheduled.
+    // The start and end times and the navigator 
+    // of the appointment being scheduled.
     var start;
     var end;
+    var navigator;
 
     // Initialize the calendar
     cal.fullCalendar({
@@ -16,9 +23,14 @@ $(document).ready(function() {
         weekends: false,
         businessHours: true,
         googleCalendarApiKey: 'AIzaSyBmErR1--htDymI-t_1CPVT7gfgg8K0yRE',
-        events: {
-            googleCalendarId: 'hsctc.org_cnp6o1rqeh2fabeg46v57oa5mo@group.calendar.google.com'
-        },
+        eventSources: [
+            {
+                googleCalendarId: 'hsctc.org_cnp6o1rqeh2fabeg46v57oa5mo@group.calendar.google.com'
+            },
+            {
+                googleCalendarId: 'cornell.edu_ksu6oqga97ppovicu81k8rruss@group.calendar.google.com'
+            }
+        ],
         eventClick: onEventClicked
     });
     
@@ -26,11 +38,12 @@ $(document).ready(function() {
     function onEventClicked(calEvent, jsEvent, view) {
         start = calEvent.start;
         end = calEvent.end;
+        navigator = calEvent.title;
         mod.modal({
             backdrop: 'static',
             keyboard: false
         });
-        text[0].innerText = 'You are currently booking an appointment for ' +
+        text[0].innerHTML = 'You are currently booking an appointment for ' +
             start.format('MMMM Do, YYYY') + ' at ' + start.format('h:mm a');
         return false;
     }
@@ -43,15 +56,25 @@ $(document).ready(function() {
             return;
         }
         // the url of my Google Apps Script, along with the GET vars.
-        var proxy = 'proxy.php?start=' + start + '&end=' + end + '&' + form.serialize();
+        var proxy = 'proxy.php?start=' + start + '&end=' + end + 
+            '&navigator=' + navigator + '&' + form.serialize();
         console.log('proxy url: ' + proxy);
         // expect a response with 'suc' and 'msg' fields.
-        $.get(proxy, function(data) {
-            if (data.suc == false) {
-                alert(data.msg);
-            }
-            document.location.reload();
-        }, 'json' );
+        $.ajax({
+            url: proxy,
+            method: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                if (data.suc == false) {
+                    alert(data.msg);
+                }
+                document.location.reload();
+            },
+            timeout: 3000,
+            error: function(jq, sts, err) {
+                alert("an unknown error occurred");
+            },
+        });
         mod.modal('hide');
         return false;
     });
--- proxy.php
+++ proxy.php
@@ -1,7 +1,18 @@
 <?php
 
-$app = 'https://script.google.com/macros/s/AKfycbyD-AO9qTpvDhLdadZl_YU9Xec0n3YkOZwteAE3qWMNLIDcV6SU/exec';
+$app = '';
+switch ($_GET['navigator']) {
+    case 'Carsten':
+        $app .= 'https://script.google.com/macros/s/AKfycbyD-AO9qTpvDhLdadZl_YU9Xec0n3YkOZwteAE3qWMNLIDcV6SU/exec';
+        break;
+    case 'Bob':
+        $app .= 'https://script.google.com/macros/s/AKfycbwwc7wQt8u6-4cPwNWd12sGUIOPYxptEocEVFxPavt91AGDXKI/exec';
+        break;
+}
+
 $host = $app . '?' . http_build_query($_GET);
+
+file_put_contents('proxy.log', $host, FILE_APPEND);
 
 $curl = curl_init();
 curl_setopt_array($curl, array(
@@ -10,5 +21,6 @@ curl_setopt_array($curl, array(
 ));
 
 $res = curl_exec($curl);
+file_put_contents('proxy.log', $res + "\n", FILE_APPEND);
 
 return json_encode($res);
